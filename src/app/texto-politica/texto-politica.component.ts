8d3a7156c196b591c9bb389b28eecd041e13d3fe
Carolina <cferrebe@emeal.nttdata.com>
2022-12-15 09:05:16 +0100

    componente texto input

--- src/app/texto-politica/texto-politica.component.ts
+++ src/app/texto-politica/texto-politica.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { datosUser } from '../interface/interfaceDatos';
 
 @Component({
   selector: 'app-texto-politica',
   templateUrl: './texto-politica.component.html',
   styleUrls: ['./texto-politica.component.css']
 })
-export class TextoPoliticaComponent {
+export class TextoPoliticaComponent implements OnInit{
+  //data of formulary component
+  @Input() datosUsuario:datosUser={
+    url:"",
+    name:"",
+    juridicPerson:"",
+    nameResponsible:"",
+    dni:"",
+    direction:"",
+    tlf:"",
+    fax:"",
+    social:"",
+    verification:false
+  }
 
+  ngOnInit(): void {
+    console.log(this.datosUsuario)
+  }
+  
 }
